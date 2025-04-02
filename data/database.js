fa4545f7271b70ffdf3f8c0f2f5b070e3627f6fd
Treasure Tamb <treasuretamb@gmail.com>
2025-04-03 00:47:58 +0200

    database mongodb version erro fix

--- data/database.js
+++ data/database.js
@@ -9,9 +9,9 @@ const initDb = (callback) => {
     console.log('Database already initialized');
     return callback(null, database);
   }
-  MongoClient.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  MongoClient.connect(process.env.MONGODB_URL)
     .then((client) => {
-      database = client.db(); // uses the default database from connection string
+      database = client.db(); 
       console.log("Connected to database:", database.databaseName);
       callback(null, database);
     })
